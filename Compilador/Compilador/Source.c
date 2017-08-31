11cbe698a6e745967e24dd0a726ba77cd5a7407c
Pedro Henrique <pedrohenriquearaujo@github.com>
2017-08-31 09:09:56 -0300

    Inicio do Scan

--- Compilador/Compilador/Source.c
+++ Compilador/Compilador/Source.c
@@ -1,18 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 #define tam 256
 
+typedef struct token{
+
+	int code;
+	char identificador[tam];
+
+}token;
+
 void Abrir_Arquivo(char nome[] , FILE * arq);
+token * scan(token * t, FILE arq, int linha, int coluna);
 
 int main(){
 
 	FILE * arquivo=NULL;
+	
+	int linha=0, coluna=0;
+	char c;
 
 	char nome_arq[tam]="arquivo.txt";
 
 	Abrir_Arquivo(nome_arq,arquivo);
 
+	while (!feof(arquivo)){
 
+		printf("Letras: %c", c);
+
+
+	}
 	
 
 
@@ -29,4 +47,37 @@ void Abrir_Arquivo(char nome[] , FILE * arq){
 	}else{
 		printf("Erro de Abertura do Arquivo!\n");
 	}
+}
+token * scan(token * t, FILE arq, int linha, int coluna){
+
+	char buffer[tam],l;
+	int i=0;
+	fread(&l,sizeof(l),1,&arq);
+
+	while (true){
+
+	 if(!isspace(l)){
+		buffer[i]=l;
+		i++;
+		checar_token(buffer,t){
+		
+		}
+	}
+	 fread(&l,sizeof(l),1,&arq);
+		
+	}
+
+
+}
+
+void checar_token(char buffer[], token * t){
+
+	enum tokens {tipo_inteiro, flutante};
+	if(strcmp(buffer,"int")==0){
+		strcpy(t->identificador,buffer);	
+		t->code=tipo_inteiro;
+	}else{
+
+	}
+
 }
